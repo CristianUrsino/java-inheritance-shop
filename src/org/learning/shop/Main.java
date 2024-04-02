c720300e4c723393818dc7844de6f407e8f536db
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-04-02 15:41:54 +0200

    updated Smartphone

--- src/org/learning/shop/Main.java
+++ src/org/learning/shop/Main.java
@@ -6,5 +6,9 @@ public class Main {
     public static void main(String[] args) {
         Product p1 = new Product("Mause" , "Tecknet" , BigDecimal.valueOf(15) , BigDecimal.valueOf(0.21));
         System.out.println(p1.getInfo());
+        Smartphone sm1 = new Smartphone("A70","Samsung", BigDecimal.valueOf(200), BigDecimal.valueOf(0.21), 248);
+        System.out.println(sm1.getInfo());
+
+
     }
 }
--- src/org/learning/shop/Smartphone.java
+++ src/org/learning/shop/Smartphone.java
@@ -1,4 +1,43 @@
 package org.learning.shop;
+//imports
+import java.math.BigInteger;
+import java.util.Random;
+import java.math.BigDecimal;
 
 public class Smartphone extends Product{
+    //attrubuti
+    private BigInteger imei;
+    private int memory;
+    //costruttori
+    public Smartphone(String name, String brand, BigDecimal price, BigDecimal vat, int memory) {
+        super(name, brand, price, vat);
+        imei = generateImei();
+        this.memory = memory;
+    }
+    //metodi per il costruttore
+    private BigInteger generateImei(){
+        Random generateRandom = new Random();
+        long tempImei;
+        do{
+            //cicla finchè non è positivo
+            //nextLong genera un numero troppo lungo, %1... per limitarlo, + 1... per dare un minimo
+            tempImei = 100000000000000L + generateRandom.nextLong() % 10000000000000L;
+        }while(tempImei<0);
+        return BigInteger.valueOf(tempImei);
+    }
+    //setter
+    public void setMemory(int memory) {
+        this.memory = memory;
+    }
+    //getter
+    public BigInteger getImei() {
+        return imei;
+    }
+    public int getMemory() {
+        return memory;
+    }
+    //altri metodi
+    public String getInfo(){
+        return super.getInfo() + "\n\timei: " + imei + "\n\tmemory: " + memory;
+    }
 }
