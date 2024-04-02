71a423284dd8ae04a94f39336350af344f894ecd
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-04-02 16:01:19 +0200

    updated headphones

--- src/org/learning/shop/Headphones.java
+++ src/org/learning/shop/Headphones.java
@@ -1,4 +1,36 @@
 package org.learning.shop;
 
+import java.math.BigDecimal;
+
 public class Headphones extends Product{
+    //attributi
+    private String color;
+    private Boolean type;//0 wired, 1 wireless
+    //costruttori
+    public Headphones(String name, String brand, BigDecimal price, BigDecimal vat, String color, Boolean type) {
+        super(name, brand, price, vat);
+        this.color = color;
+        this.type = type;
+    }
+    //setter
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setType(Boolean type) {
+        this.type = type;
+    }
+    //getter
+    public String getColor() {
+        return color;
+    }
+    public Boolean getType() {
+        return type;
+    }
+    //altri metodi
+    public String getTypeText(){
+        return type ? "wired" : "wireless";
+    }
+    public String getInfo(){
+        return super.getInfo() + "\n\tcolor: " + color + "\n\ttype: " + getTypeText();
+    }
 }
--- src/org/learning/shop/Main.java
+++ src/org/learning/shop/Main.java
@@ -10,6 +10,8 @@ public class Main {
         System.out.println(sm1.getInfo());
         Television tv1 = new Television("pippo", "Q.Bell", BigDecimal.valueOf(150), BigDecimal.valueOf(0.21), BigDecimal.valueOf(19.24), true);
         System.out.println(tv1.getInfo());
+        Headphones hp1 = new Headphones("mii2", "realme", BigDecimal.valueOf(20), BigDecimal.valueOf(0.21), "black", true);
+        System.out.println(hp1.getInfo());
 
     }
 }
