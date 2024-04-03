cbe827b283d5ce2e73c2a8803b54f628f32eacae
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-04-03 10:21:27 +0200

    updated Product: added discount and calculateDiscount, updated getInfo

--- src/org/learning/shop/Product.java
+++ src/org/learning/shop/Product.java
@@ -10,13 +10,17 @@ public class Product {
     private BigDecimal price;
     private BigDecimal vat;
 
+    protected BigDecimal discount = new BigDecimal(0.02);
+    private boolean isRegistered;
+
     //costruttori
-    public Product(String name, String brand, BigDecimal price, BigDecimal vat) {
+    public Product(String name, String brand, BigDecimal price, BigDecimal vat, boolean isRegistered) {
         productCode = generateProductCode();
         this.name = name;
         this.brand = brand;
         this.price = price;
         this.vat = vat;
+        this.isRegistered = isRegistered;
     }
     //metodi per il costruttore
     private int generateProductCode(){
@@ -55,14 +59,39 @@ public class Product {
     //altri metodi
     public BigDecimal getTotalPrice(){
         //la formula è: price * (1 + iva)
-        return price.multiply(BigDecimal.ONE.add(vat));
+        if(!isRegistered){
+            //se non registrato ritorno il prezzo
+            return price.multiply(BigDecimal.ONE.add(vat));
+        }
+        else{
+            //altrimenti
+            return calculateDiscount().multiply(BigDecimal.ONE.add(vat));
+        }
     }
     public String getInfo(){
-        //ritorno codice-nome, ma il codice anche se di 4 cifre appare di 8 (es. 00001234)
+        //se registrato creo un testo extra da ritornare
+        String discountText = "";
+        if(this.isRegistered){
+            discountText =
+                    "\n\tdiscount: " + discount.setScale(2,BigDecimal.ROUND_HALF_UP) +
+                    "\n\tprice discounted: " + calculateDiscount().setScale(2,BigDecimal.ROUND_HALF_UP);
+        }
+        //ritorno le informazioni formattate
         return String.format("%08d",productCode) + "-" + name + ": " +
                 "\n\tbrand: " + brand +
                 "\n\tprice: " + price +
+                (isRegistered ? discountText : "") +
                 "\n\tvat: " + vat +
-                "\n\tfull price: " + getTotalPrice();
+                "\n\tfull price: " + getTotalPrice().setScale(2,BigDecimal.ROUND_HALF_UP);
+    }
+    private BigDecimal calculateDiscount(){
+        if(!isRegistered){
+            //se l'utente non è registrato mando un output di errore e ritorno il prezzo normale
+            System.out.println("Error you aren't registered");
+            return price;
+        }else {
+            //prezzo * 1-sconto //(100 * 0.98)
+            return price.multiply(BigDecimal.ONE.subtract(discount));
+        }
     }
 }
